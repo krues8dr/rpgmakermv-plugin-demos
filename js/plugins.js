bee22b90afb901237c3afd07cfecd9eb6ff34b5e
Bill Hunt <bill@krues8dr.com>
2019-05-27 09:55:20 -0400

    Adding plugins for gameseed.

--- js/plugins.js
+++ js/plugins.js
@@ -3,5 +3,7 @@
 var $plugins =
 [
 {"name":"Kru_Demo","status":true,"description":"1.0.0 Helper for demos of plugins.","parameters":{"Path":"/demo/shared/","Container":"game"}},
-{"name":"Kru_Core","status":true,"description":"1.0.0 Shared base for many Krues8dr plugins","parameters":{"---General---":""}}
+{"name":"Kru_Core","status":true,"description":"1.0.0 Shared base for many Krues8dr plugins","parameters":{"---General---":""}},
+{"name":"HIME_CustomPageConditions","status":true,"description":"v1.4 - Create your own page conditions for your events using\r\nregular eventing!","parameters":{}},
+{"name":"Kru_GameSeed","status":true,"description":"v1.0.0 Creates a custom random seed for the game.","parameters":{"Seed Variable Name":"6"}}
 ];
